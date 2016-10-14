991ce3c5ccee779a7ff50a96e2892dcaf3f4c468
Rafael William <rafaw940@yahoo.com.br>
2016-10-14 11:35:34 -0300

    comentato codigo fabio

--- public/javascripts/demo-controller.js
+++ public/javascripts/demo-controller.js
@@ -2,25 +2,18 @@
 
 	var app = angular.module('xamarin-app');
 
-	app.controller('DemoCtrl', function($http, $location, $interval) {
+	app.controller('DemoCtrl', function($http, $location,localStorageService,$interval) {
 		var url = 'https://xamarin-api.herokuapp.com/api';
-//<<<<<<< HEAD
-//=======
-
-//>>>>>>> eecc2b7801c7d3208ce56bd3ce75d078e7b27be2
 		var interval = $interval(function() {
 			chkSorteio();
 		}, 3000);
-
 		chkSorteio();
-
-		function chkSorteio() {
+		function chkSorteio(){
 			$http({
 				method: 'GET',
 				url: url+'/raffle',
 			}).success(function(data, status ) {
 				$location.path('/bolinha');
-//<<<<<<< HEAD
 				$interval.cancel(interval);
 			   	//$("#loading").fadeOut(500);		
 			   	//$("body").html('<h1>'+data.email+'</h1>');				
@@ -29,17 +22,10 @@
 				}else{
 					$location.path('/resultado');
 				}
-//=======
-				$interval.cancel(interval)
-			   //	$("#loading").fadeOut(500);
-			   	//$("body").html('<h1>'+data.email+'</h1>');
-
-//>>>>>>> eecc2b7801c7d3208ce56bd3ce75d078e7b27be2
 
 		 	}).error(function(data, status) {
 				if (status === 400) {
 					// TODO subir erro de e-mail duplicado
-//<<<<<<< HEAD
 				}else{
 					// TODO erro da aplicação
 				}	
@@ -47,13 +33,4 @@
 			}
 		});
 	
-//=======
-				} else {
-					// TODO erro da aplicação
-				}
-			});
-		}
-});
-
-//>>>>>>> eecc2b7801c7d3208ce56bd3ce75d078e7b27be2
 })(angular);
