b47273f0505e68e27430f2fea323458df8293de1
Rafael William <rafaw940@yahoo.com.br>
2016-10-20 12:25:17 -0200

    redirect demo ajuste

--- public/javascripts/ganhador-controller.js
+++ public/javascripts/ganhador-controller.js
@@ -5,6 +5,33 @@
 	app.controller('GanhadorCtrl', function($location, $sessionStorage) {
 		if (!$sessionStorage.winner && !$sessionStorage.email) $location.path('/');
 		if (!$sessionStorage.winner && $sessionStorage.email) $location.path('/demo'+Math.floor((Math.random() * 30) + 1));
+
+		var interval = $interval(function() {
+			chkEmail();
+		}, 3000);
+
+		chkEmail();
+
+		function chkEmail(){
+			$http({
+				method: 'GET',
+				url: url+'/email',
+			}).success(function(data, status) {
+				
+
+				$interval.cancel(interval);
+
+				var path = undefined;
+				if(data.length == 0){
+				$sessionStorage.email = null;
+				$sessionStorage.winner = null;
+				}
+		 	}).error(function(data, status) {
+				if (status === 403) {
+					console.log('Sorteio NÃO efetuado!');
+				}
+			});
+
 	});
 
 })(angular);
